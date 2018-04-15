187b80d6c0cbe884ddca7d39042ee3b6af5462cc
Tauseef Ahmed <ahmad.tauseef2@gmail.com>
2018-04-15 17:43:40 +0530

    Validation added

--- src/main/java/com/ubs/opsit/interviews/TimeConverterImpl.java
+++ src/main/java/com/ubs/opsit/interviews/TimeConverterImpl.java
@@ -4,6 +4,9 @@ public class TimeConverterImpl implements TimeConverter {
 
 	@Override
 	public String convertTime(String aTime) {
+		// Validate time
+		validate(aTime);
+		
 		StringBuilder result = new StringBuilder();
 
 		String[] timeSplitted = aTime.split(":");
@@ -17,54 +20,58 @@ public class TimeConverterImpl implements TimeConverter {
 		int noOfMinutes = minutes % 5;
 
 		// first row to display seconds
-		if (seconds % 2 == 0) {
-			result.append("Y");
-		} else {
-			result.append("O");
-		}
+		result.append((seconds % 2 == 0)?"Y":"O");
 		result.append("\r\n");
-		// second row to display 5 hours 
+		
+		// second row to display 5 hours
 		for (int i = 0; i < 4; i++) {
-			if (i < noOf5Hours) {
-				result.append("R");
-			} else {
-				result.append("O");
-			}
+			result.append((i < noOf5Hours)?"R":"O");
 		}
 		result.append("\r\n");
+		
 		// third row to display hours
 		for (int i = 0; i < 4; i++) {
-			if (i < noOfHours) {
-				result.append("R");
-			} else {
-				result.append("O");
-			}
+			result.append((i < noOfHours)?"R":"O");
 		}
 		result.append("\r\n");
+		
 		// fourth row to display 5 minutes
 		for (int i = 0; i < 11; i++) {
-			if (i < noOf5Minutes) {
-				if (i == 2 || i == 5 || i == 8) {
-					result.append("R");
-				} else {
-					result.append("Y");
-				}
-			} else {
-				result.append("O");
-			}
+			result.append((i < noOf5Minutes)?(i == 2 || i == 5 || i == 8)?"R":"Y":"O");
 		}
 		result.append("\r\n");
+		
 		// fifth row to display minutes
 		for (int i = 0; i < 4; i++) {
-			if (i < noOfMinutes) {
-				result.append("Y");
-			} else {
-				result.append("O");
-			}
+			result.append((i < noOfMinutes)?"Y":"O");
 		}
-
-		// System.out.println(result);
 		return result.toString();
 	}
 
+	private void validate(String aTime) {
+
+		if (null == aTime) {
+			throw new RuntimeException("Time not provided.");
+		}
+
+		String[] timeSplitted = aTime.split(":");
+
+		if (timeSplitted.length != 3) {
+			throw new RuntimeException("Time format is wrong.");
+		}
+
+		int hours, minutes, seconds = 0;
+
+		try {
+			hours = Integer.parseInt(timeSplitted[0]);
+			minutes = Integer.parseInt(timeSplitted[1]);
+			seconds = Integer.parseInt(timeSplitted[2]);
+		} catch (NumberFormatException e) {
+			throw new RuntimeException("Time not in numbers");
+		}
+
+		if (hours < 0 || hours > 24 || minutes < 0 || minutes > 59 || seconds < 0 || seconds > 59) {
+			throw new RuntimeException("Time is out of bound");
+		}
+	}
 }
