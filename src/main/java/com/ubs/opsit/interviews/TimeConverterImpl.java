ffea14053da5f4eca42898b0a96d9fa6a31689f0
Tauseef Ahmed <ahmad.tauseef2@gmail.com>
2018-04-17 00:14:13 +0530

    enums added for display

--- src/main/java/com/ubs/opsit/interviews/TimeConverterImpl.java
+++ src/main/java/com/ubs/opsit/interviews/TimeConverterImpl.java
@@ -1,6 +1,13 @@
 package com.ubs.opsit.interviews;
 
 public class TimeConverterImpl implements TimeConverter {
+	
+	
+	// Display bits/colors
+	private enum Display{
+		O,Y,R
+	}
+	
 
 	@Override
 	public String convertTime(String aTime) {
@@ -20,30 +27,30 @@ public class TimeConverterImpl implements TimeConverter {
 		int noOfMinutes = minutes % 5;
 
 		// first row to display seconds
-		result.append((seconds % 2 == 0)?"Y":"O");
+		result.append((seconds % 2 == 0)?Display.Y:Display.O);
 		result.append("\r\n");
 		
 		// second row to display 5 hours
 		for (int i = 0; i < 4; i++) {
-			result.append((i < noOf5Hours)?"R":"O");
+			result.append((i < noOf5Hours)?Display.R:Display.O);
 		}
 		result.append("\r\n");
 		
 		// third row to display hours
 		for (int i = 0; i < 4; i++) {
-			result.append((i < noOfHours)?"R":"O");
+			result.append((i < noOfHours)?Display.R:Display.O);
 		}
 		result.append("\r\n");
 		
 		// fourth row to display 5 minutes
 		for (int i = 0; i < 11; i++) {
-			result.append((i < noOf5Minutes)?(i == 2 || i == 5 || i == 8)?"R":"Y":"O");
+			result.append((i < noOf5Minutes)?(i == 2 || i == 5 || i == 8)?Display.R:Display.Y:Display.O);
 		}
 		result.append("\r\n");
 		
 		// fifth row to display minutes
 		for (int i = 0; i < 4; i++) {
-			result.append((i < noOfMinutes)?"Y":"O");
+			result.append((i < noOfMinutes)?Display.Y:Display.O);
 		}
 		return result.toString();
 	}
